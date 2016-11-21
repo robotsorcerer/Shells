81714bc3e15466f21047d02bd7f4a7595e439410
lakehanne <patlekano@gmail.com>
2016-11-21 13:25:49 -0600

    updated based on cuda8.0 run install

--- gitsetup.sh
+++ gitsetup.sh
@@ -8,24 +8,3 @@ git config --list			# will display the above info in terminal
 printf '\n\n Cache the password\n\n'
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
-
-#do git push mode to matching
-git config --global push.default matching
-
-#add ssh keygen
-ssh-keygen -t rsa -b 4096 -C "patlekano@gmail.com"
-wait
-
-#add key to  ssh agent
-eval "$(ssh-agent -s)"
-
-ssh-add ~/.ssh/id_rsa
-
-if hash xclip 2>/dev/null; then
-    xclip -sel clip < ~/.ssh/id_rsa.pub
-else
-    install xclip;
-    xclip -sel clip < ~/.ssh/id_rsa.pub
-fi
-
-echo "you may now paste the ssh keys in your window"
