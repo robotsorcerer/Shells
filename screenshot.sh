ab38482cc6c25e77ea4a98a3b96393b6b97eb9d6
lakehanne <patlekano@gmail.com>
2019-02-15 14:01:18 -0600

    automated rok80 swmed

---
+++ screenshot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Create a global gitignore for macOS
+curl https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore >> ~/.gitignore_global 
+git config --global core.excludesfile ~/.gitignore_global
+
+# Send screenshots to a directory that isn't the desktop
+mkdir -p ~/Screenshots
+defaults write com.apple.screencapture location ~/Screenshots
+
+# Show all hidden files (like dotfiles)
+defaults write com.apple.finder AppleShowAllFiles YES; killall Finder;
+
+# DANGERZONE: Disable Gatekeeper permanently to allow opening apps from unidentified developers 
+sudo defaults write /Library/Preferences/com.apple.security GKAutoRearm -bool NO
+
+# Disable smart quotes to save yourself from pesky syntax errors when copy pasting from Notes/TextEdit
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
